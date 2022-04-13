2b8ee743a1de7f8c5f1e5b22ff47f4a056848e84
Μπάμπης <99083127+Mpamphs1@users.noreply.github.com>
2022-04-13 21:39:57 +0300

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,9 @@
+name=$(whoami)
+ip=$(ip -6 addr)
+
+echo "Geia sou $name molis eklepsa thn ip sou pou einai $ip"
+sleep 1
+echo "se ligo 8a mpo sthn trapeza sou, ara pane ta 500 euro pou exeis ftoxe"
+sleep 1
+echo "gia na glitosoume xrono apla grapse edo thn trapeza sou na teleionoume."
+read trapeza
